e0fbe520445108322dea25ce5a8304bdc55d222a
Fernando Ferreira <fm_ferreira1996@hotmail.com>
2022-07-12 22:09:56 +0100

    Initial draft of rank progress bar display

--- init.lua
+++ init.lua
@@ -38,27 +38,72 @@ function StylishCombat:new()
                 StylishCombat.displayStyleMeter = false
             end
         end)
+
+        Observe('PlayerPuppet', 'OnDeath', function(self, event)
+            if StylishCombat.displayStyleMeter then
+                GameHUD.ShowWarning("You've done poorly", 2) -- Should have several possible disses
+            end
+
+            StylishCombat.displayStyleMeter = false
+        end)
+
+        -- Hide on any menu
     end)
 
     registerForEvent("onDraw", function()
-        if StylishCombat.displayStyleMeter then
-            local screenWidth, screenHeight = GetDisplayResolution()
-            local width  = 400
-            local height = 250
+        if not StylishCombat.displayStyleMeter then return end
 
-            ImGui.SetNextWindowPos(screenWidth - (width * 1.2), screenHeight * 0.9 / 2, ImGuiCond.Always)
-            ImGui.SetNextWindowSize(width, height, ImGuiCond.Appearing)
+        local screenWidth, screenHeight = GetDisplayResolution()
+        local width  = 400 * (screenWidth / 3440)
+        local height = 100 * (screenHeight / 1440)
 
-            CPS:setThemeBegin()
+        local styleAmount = 50 -- percentage of style meter to be filled
 
-            if ImGui.Begin("Style Ranking") then
-                ImGui.Text("Stylish af!")
-            end
+        ImGui.SetNextWindowPos(screenWidth - (width * 1.25), screenHeight * 0.85 * 0.5, ImGuiCond.Always)
+        ImGui.SetNextWindowSize(width, height, ImGuiCond.Appearing)
 
-            ImGui.End()
+        CPS:setThemeBegin()
 
-            CPS:setThemeEnd()
+        if ImGui.Begin("Style Ranking", true, ImGuiWindowFlags.NoResize + ImGuiWindowFlags.NoMove +  ImGuiWindowFlags.NoTitleBar + ImGuiWindowFlags.NoScrollbar + ImGuiWindowFlags.NoBackground) then
+            -- bigger with each level?
+            local fontScale = 2 -- 2.5
+            ImGui.SetWindowFontScale(fontScale)
+
+            local text = "Dull"
+
+            local textSizeX, _textSizeY = ImGui.CalcTextSize(text)
+
+            ImGui.SetCursorPos((width - textSizeX) * 0.5, 0)
+
+            -- Label
+            CPS.colorBegin("Text", 0xFF4CFFFF)
+
+            -- needs to depend on style rank
+            ImGui.Text(text)
+
+            CPS.colorEnd(1)
+
+            CPS.styleBegin("FrameRounding", 11)
+
+            -- Needs a style level
+            -- Get colour from style level
+            CPS.colorBegin("FrameBg", 0x9926214A)
+            CPS.colorBegin("PlotHistogram", 0xE64547C7)
+
+            ImGui.SetCursorPos(0, ImGui.GetFontSize() * 1.6)
+
+            ImGui.ProgressBar(styleAmount * 0.01, width, height - (ImGui.GetFontSize() * 1.6), "")
+
+            -- count must match the number of pushes above
+            CPS.colorEnd(2)
+            CPS.styleEnd(1)
+
+            ImGui.SetWindowFontScale(1)
         end
+
+        ImGui.End()
+
+        CPS:setThemeEnd()
     end)
 
     return StylishCombat
